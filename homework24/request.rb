7f42cf55cd11265d975261a1ac7346ab5a6fe83d
Andrew <Andreymg184@gmai.com>
2020-07-30 18:01:43 +0600

    request class and interface

---
+++ homework24/request.rb
@@ -0,0 +1,36 @@
+require 'net/http'
+
+class Request
+  attr_reader :uri, :response
+
+  def get(url, headers = {})
+    http = init_http(url)
+
+    request = Net::HTTP::Get.new(uri)
+    headers.each { |key, value| request[key] = value }
+
+    @response = http.request(request)
+  end
+
+  def res_status
+    response&.code
+  end
+
+  def res_body
+    response&.body
+  end
+
+  def res_headers
+    response&.each_header {}
+  end
+
+  private
+
+  def init_http(url)
+    @uri = URI(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true if uri.instance_of?(URI::HTTPS)
+
+    http
+  end
+end
